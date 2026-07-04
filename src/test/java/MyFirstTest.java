35b3aa8bc14dd9a66ef412724b19b92383cca4fb
mdibrova <msekina@yandex.ru>
2026-07-04 17:30:55 +0300

    Мой второй коммит

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.sleep;
 
 public class MyFirstTest {
+    @Test
+    void test01WebDriver() {
+        open("https://ru.wikipedia.org/wiki/Selenium");
+        sleep(10_000);
+        $(By.tagName("body")).shouldHave(text("WebDriver"));
+
+    }
+
+    //@Test
+    //void test02Selenide() {
+        //open("https://ru.wikipedia.org/wiki/Selenium");
+        //sleep(10_000);
+        //$(By.tagName("body")).shouldHave(text("Selenide"));
+    //}
 }
+
