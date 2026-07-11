92810c9bff952571b7d6d29360458f05edec7f22
mdibrova <msekina@yandex.ru>
2026-07-11 13:57:15 +0300

    Проверка тегов

--- src/test/java/SearchElementTest.java
+++ src/test/java/SearchElementTest.java
@@ -8,14 +8,18 @@ import static com.codeborne.selenide.Selenide.*;
 public class SearchElementTest {
     @Test
     void test00SearchId00() {
+        //На странице https://www.saucedemo.com/ в поле Username ввести своё имя.
         open("https://www.saucedemo.com");
         $(By.id("user-name")).sendKeys("Мария");
         sleep(3_000);
-
     }
 
+    //На странице https://slqamsk.github.io/demo/search-demo/ найти сначала в DevTools,
+    // а потом с помощью автотеста указанные элементы, проверить, что они видимы на странице,
+    // используя команду (метод): .shouldBe(visible);
     @Test
     void test01SearchId01() {
+        //Поиск по ID элемента с идентификатором submit-button
         open("https://slqamsk.github.io/demo/search-demo");
         $(By.id("submit-button")).shouldBe(visible);
         sleep(3_000);
@@ -23,6 +27,7 @@ public class SearchElementTest {
 
     @Test
     void test01SearchId02() {
+        //Поиск по ID элемента с идентификатором user-password
         open("https://slqamsk.github.io/demo/search-demo");
         $(By.id("user-password")).shouldBe(visible);
         sleep(3_000);
@@ -30,6 +35,7 @@ public class SearchElementTest {
 
     @Test
     void test02SearchName01() {
+        //Поиск по атрибуту name (поиск по имени элемента) по атрибуту name="interests"
         open("https://slqamsk.github.io/demo/search-demo");
         $(By.name("interests")).shouldBe(visible);
         sleep(3_000);
@@ -37,6 +43,7 @@ public class SearchElementTest {
 
     @Test
     void test02SearchName02() {
+        //Поиск по атрибуту name (поиск по имени элемента) по атрибуту name="user_email"
         open("https://slqamsk.github.io/demo/search-demo");
         $(By.name("user_email")).shouldBe(visible);
         sleep(3_000);
@@ -44,6 +51,7 @@ public class SearchElementTest {
 
     @Test
     void test03SearchClass01() {
+        //Поиск по имени класса элемента с классом nav-link
         open("https://slqamsk.github.io/demo/search-demo");
         $(By.className("nav-link")).shouldBe(visible);
         sleep(3_000);
@@ -51,6 +59,7 @@ public class SearchElementTest {
 
     @Test
     void test03SearchClass02() {
+        //Поиск по имени класса элемента с классом btn
         open("https://slqamsk.github.io/demo/search-demo");
         $(By.className("btn")).shouldBe(visible);
         sleep(3_000);
@@ -58,6 +67,7 @@ public class SearchElementTest {
 
     @Test
     void test04SearchTagname01() {
+        //Поиск по имени тега поля для ввода текста на странице (тег input)
         open("https://slqamsk.github.io/demo/search-demo");
         $(By.tagName("input")).shouldBe(visible);
         sleep(3_000);
@@ -65,6 +75,7 @@ public class SearchElementTest {
 
     @Test
     void test04SearchTagname02() {
+        //Поиск по имени тега заголовка третьего уровня (тег h3)
         open("https://slqamsk.github.io/demo/search-demo");
         $(By.tagName("h3")).shouldBe(visible);
         sleep(3_000);
@@ -72,6 +83,7 @@ public class SearchElementTest {
 
     @Test
     void test05SearchLink01() {
+        //Поиск по полному тексту ссылки с точным текстом "Контакты и обратная связь"
         open("https://slqamsk.github.io/demo/search-demo");
         $(By.linkText("Контакты и обратная связь")).shouldBe(visible);
         sleep(3_000);
@@ -79,6 +91,7 @@ public class SearchElementTest {
 
     @Test
     void test05SearchLink02() {
+        //Поиск по полному тексту ссылки с точным текстом "Регистрация нового пользователя в системе"
         open("https://slqamsk.github.io/demo/search-demo");
         $(By.linkText("Регистрация нового пользователя в системе")).shouldBe(visible);
         sleep(3_000);
@@ -86,6 +99,7 @@ public class SearchElementTest {
 
     @Test
     void test06SearchPartiallink01() {
+        //Поиск по частичному тексту ссылки, содержащему слово "длинный"
         open("https://slqamsk.github.io/demo/search-demo");
         $(By.partialLinkText("длинный")).shouldBe(visible);
         sleep(3_000);
@@ -93,6 +107,7 @@ public class SearchElementTest {
 
     @Test
     void test06SearchPartiallink02() {
+        //Поиск по частичному тексту ссылки, содержащему слово "Контакты"
         open("https://slqamsk.github.io/demo/search-demo");
         $(By.partialLinkText("Контакты")).shouldBe(visible);
         sleep(3_000);
